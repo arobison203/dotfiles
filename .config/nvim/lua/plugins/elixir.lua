1760045537956edc2b5b061e8dac40df86cc2087
Alexandra Robison <alr@ar6.io>
2025-01-17 17:03:35 -0700

    clean up elixir config for neovim

--- .config/nvim/lua/plugins/elixir.lua
+++ .config/nvim/lua/plugins/elixir.lua
@@ -3,6 +3,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     optional = false,
     opts = function(_, opts)
+      -- ensure the language server is installed
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "elixirls" })
     end,
   },
@@ -11,41 +12,10 @@ return {
     optional = false,
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "elixir", "heex" })
+        opts.ensure_installed =
+          -- ensure tresitter has the required parsers installed
+          require("astrocore").list_insert_unique(opts.ensure_installed, { "elixir", "eex", "heex", "html" })
       end
     end,
   },
-  -- {
-  --   "AstroNvim/astrolsp",
-  --   optional = false,
-  --   opts = {
-  --     config = {
-  --       tailwindcss = {
-  --         root_dir = function(fname)
-  --           local root_pattern = require("lspconfig").util.root_pattern(
-  --             "tailwind.config.js",
-  --             "assets/tailwind.config.js",
-  --             "apps/api/assets/tailwind.config.cjs" -- this is for an umbrella elixir project
-  --           )
-  --           return root_pattern(fname)
-  --         end,
-  --         init_options = {
-  --           userLanguages = {
-  --             heex = "html-eex",
-  --             elixir = "html-eex",
-  --           },
-  --         },
-  --         settings = {
-  --           tailwindCSS = {
-  --             experimental = {
-  --               classRegex = {
-  --                 'class[:]\\s*"([^"]*)"',
-  --               },
-  --             },
-  --           },
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
 }
